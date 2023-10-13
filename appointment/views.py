b84a270362ac105226217e0ed2d3fb35b59d2f90
shivangini <kiran04252@gmail.com>
2023-10-13 10:59:08 +0530

    creating and sending mail

--- appointment/views.py
+++ appointment/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-# Create your views here.
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from .models import *
+from django.core.mail import send_mail
+from django.conf import settings
+# Create your views here.
+
 
 @csrf_exempt
 def details(request):
@@ -17,4 +19,9 @@ def details(request):
         subject = request.POST['subject']
         user = Appointment.objects.create( fullname=fullname, phone=phone, email=email, date=date,subject=subject)
         user.save()
+        subject = 'Subject of the Email'
+        message = 'Body of the email.'
+        from_email = settings.EMAIL_HOST_USER
+        to_email = ['kiran04252@gmail.com']  # Replace with the recipient's email address
+        send_mail(subject, message, from_email, to_email)
     return HttpResponse("Successfully submitted", status=200)
